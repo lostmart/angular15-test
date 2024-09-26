6ff9a9ff76e104549dd62d0dbf4883b3d0a9f70c
lostmart <lostmart@hotmail.com>
2024-09-26 20:37:19 +0200

    nunca temas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { PokemonItemComponent } from './components/pokemon-tem.component';
-import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 
 @NgModule({
-  declarations: [AppComponent,
-    PokemonItemComponent,
-    PokemonListComponent
-  ],
+  declarations: [AppComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],

17535490b8a560051b093b00aea09c8f4dfcde56
lostmart <lostmart@hotmail.com>
2024-09-26 20:42:02 +0200

    nunca temas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+type CarType = 'sport' | 'classic';
+
+export type Car = {
+  carName: string;
+  type: CarType;
+};
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'titulo pelotudo';
+  title = 'These are cars';
 }

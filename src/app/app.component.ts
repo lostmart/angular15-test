a4eba4b36dd7a5733f7c741ca6fce6c1296b7189
lostmart <martinpedraza1979@gmail.com>
2024-09-27 12:19:05 +0200

    links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { TCar, CarService } from './services/car.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-
-}
+export class AppComponent {}

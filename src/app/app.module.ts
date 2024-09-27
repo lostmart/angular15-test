358237ab253eac11be457b64b823d41c7b2c2f2e
lostmart <martinpedraza1979@gmail.com>
2024-09-27 17:03:22 +0200

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
         component: GeneralComponent,
       },
       {
-        path: '',
+        path: 'details',
         component: DetailsComponent,
       },
     ],

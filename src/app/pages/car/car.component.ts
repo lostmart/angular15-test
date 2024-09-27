e1117d65a0e6b7b85e167e38326b09fff384abe2
lostmart <martinpedraza1979@gmail.com>
2024-09-27 12:31:31 +0200

    programmatic navigation after 2 secs

--- src/app/pages/car/car.component.ts
+++ src/app/pages/car/car.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
-  styleUrls: ['./car.component.css']
+  styleUrls: ['./car.component.css'],
 })
 export class CarComponent {
+  constructor(private router: Router) {
+    setTimeout(() => {
+      this.goHome();
+    }, 2000);
+  }
 
+  goHome() {
+    this.router.navigate(['../']);
+  }
 }

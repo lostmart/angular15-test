1f3739896cfc8305b78d3a28c4bcde41805ffb85
lostmart <martinpedraza1979@gmail.com>
2024-09-27 12:16:46 +0200

    new car page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,21 @@ import { FormsModule } from '@angular/forms';
 import { CarItemComponent } from './components/car-item/car-item.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { CarComponent } from './pages/car/car.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
   },
+  {
+    path: 'car',
+    component: CarComponent,
+  },
 ];
 
 @NgModule({
-  declarations: [AppComponent, CarItemComponent, HomeComponent],
+  declarations: [AppComponent, CarItemComponent, HomeComponent, CarComponent],
   imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
